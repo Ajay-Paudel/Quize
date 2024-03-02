7905ea341949ed4db92f9b21b75fa901beaff281
Ajay-Paudel <ajayindrapaudel@gmail.com>
2024-03-02 14:51:21 +0530

    first commit

---
+++ app/src/main/java/com/example/quize_app/QuestionAnswer.java
@@ -0,0 +1,35 @@
+package com.example.quize_app;
+
+public class QuestionAnswer {
+
+    public static String question[] = {
+            "What is 10 + 15?",
+            "Who is the author of 'Romeo and Juliet'?",
+            "What is the capital of France?",
+            "Which planet is known as the Red Planet?",
+            "What is the largest mammal in the world?",
+            "Which country is known as the Land of the Rising Sun?",
+            "What is the chemical symbol for gold?",
+            "Who painted the Mona Lisa?",
+            "What is the tallest mountain in the world?",
+            "Which element has the symbol 'O' in the periodic table?"
+    };
+
+    public static String choices[][] = {
+            {"24", "25", "35", "34"},
+            {"William Shakespeare", "Charles Dickens", "Jane Austen", "Mark Twain"},
+            {"Berlin", "Madrid", "Paris", "Rome"},
+            {"Venus", "Mars", "Jupiter", "Earth"},
+            {"Elephant", "Blue Whale", "Giraffe", "Hippopotamus"},
+            {"China", "South Korea", "Japan", "Vietnam"},
+            {"Au", "Ag", "Fe", "Pt"},
+            {"Leonardo da Vinci", "Vincent van Gogh", "Pablo Picasso", "Michelangelo"},
+            {"Mount Kilimanjaro", "Mount Everest", "Mount McKinley", "Mount Fuji"},
+            {"Oxygen", "Osmium", "Ozone", "Osmosis"}
+    };
+
+    public static String CorrectAnswers[] = {
+            "25", "William Shakespeare", "Paris", "Mars", "Blue Whale",
+            "Japan", "Au", "Leonardo da Vinci", "Mount Everest", "Oxygen"
+    };
+}
